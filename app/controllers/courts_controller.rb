4978e5a480177c8fb9df39333c876416abc2bcde
Xavier Pardoue <pardoue.xavier@gmail.com>
2026-03-11 17:29:02 +0100

    methodes courts controller et elements dans la view

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,4 +1,7 @@
 class CourtsController < ApplicationController
   def show
+    @court = Court.find(params[:id])
+    @meets = Meet.where(court_id: @court.id)
+    @users = User.joins(:victories).group("users.id").order("COUNT(victories.id) DESC")
   end
 end
