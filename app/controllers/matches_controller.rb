d12486c565e929528d63fb717b8bd22c348e97f2
Micka <hibon.mickael@gmail.com>
2026-03-11 15:58:28 +0100

    Fix comment formatting in MatchesController

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,7 @@
 class MatchesController < ApplicationController
   def new
   @match = Match.new
-  #pour créer un objet meet vide associé à match
+  # pour créer un objet meet vide associé à match
   @match.build_meet
   end
 
