195f5c7669fe0bcd97f567cb157d9c78d462576b
mickael hibon <hibon.mickael@gmail.com>
2026-03-14 12:24:40 +0100

    Components css, timer for flashes

--- app/javascript/controllers/flashes_controller.js
+++ app/javascript/controllers/flashes_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flashes"
 export default class extends Controller {
   connect() {
+    setTimeout(() => this.element.remove(), 3000) // 3 secondes avant de supprimer l'alerte
   }
 
   close() {
@@ -10,4 +11,3 @@ export default class extends Controller {
   }
 
 }
-
