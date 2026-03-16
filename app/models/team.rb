d1c789ad562dde094ce227bce4e3619a169d78f2
Xavier Pardoue <pardoue.xavier@gmail.com>
2026-03-16 16:56:18 +0100

    Correction du formulaire

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   has_many :programs
   has_many :user_teams
   has_many :users, through: :user_teams
-  validates :number_player, presence: true
+  validates :number_player, presence: true, numericality: { greater_than_or_equal_to: 1, message: "doit être au minimum 1 (1v1)" }
 end
