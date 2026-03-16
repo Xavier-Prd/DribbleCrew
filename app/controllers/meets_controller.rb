b96619384cd7140c78a4cb88efb9d71b25d2bb24
Pierre Planex <pierre.planex@gmail.com>
2026-03-16 12:03:40 +0100

    entrainement rejoignable

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -8,7 +8,7 @@ class MeetsController < ApplicationController
   # 2. Tous les programs de l'user
   @programs = current_user.programs
 
-  # 3. Les rencontres passées 
+  # 3. Les rencontres passées
   @past_meets = Meet.includes(:court, :meetable)
                     .where("date < ?", Time.current)
                     .order(date: :desc)
@@ -16,54 +16,79 @@ class MeetsController < ApplicationController
 
   def show
     @meet = Meet.find(params[:id])
-    # si le meetable (polymorphic) est un programme alors je redirige vers la page du programme
     if @meet.meetable_type == "Program"
-    return redirect_to program_path(@meet.meetable)
-    end
+      # Au lieu de rediriger, on récupère le programme pour l'afficher dans la vue du Meet
+      @program = @meet.meetable
+    else
     # meetable (polymorphic) est un match donc je récupére l'obet match
     @match = @meet.meetable
     # il faut que je calcule le nombre de joeur par équipe (équipe rouge et équipe bleu )
     @current_players_count = @match.blue_team.users.count + @match.red_team.users.count
+    end
   end
 
 
-
   def join
-    @meet = Meet.find(params[:id])
-    @team = Team.find(params[:team_id])
-    # pour pouvoir mettre un marqueur sur l'équipe que je vais choisir
-    @match = @meet.meetable
+   @meet = Meet.find(params[:id])
+   @meetable = @meet.meetable
 
-    # j'empeche le créateur du match de rejoindre l'équipe rouge
-    if @match.user == current_user
-    return redirect_to meet_path(@meet), alert: "En tant qu'organisateur, tu appartiens à l'équipe bleue."
-    end
+  # Cas 1 : Entrainement
+    if @meet.meetable_type == "Program"
+    # On récupère l'équipe unique du programme
+    @team = @meetable.team
 
-    # j'empeche un utilisateur de rejoindre une équipe s'il est déjà dans l'équipe rouge ou bleu
-    if @match.blue_team.users.include?(current_user) || @match.red_team.users.include?(current_user)
-    return redirect_to meet_path(@meet), alert: "Tu es déjà inscrit à ce match !"
-    end
+        if @team.users.include?(current_user)
+        return redirect_to meet_path(@meet), alert: "Tu es déjà inscrit à cet entraînement !"
+        end
 
-    # j'empeche un utilisateur de rejoindre une équipe si l'équipe est déjà complète
-    if @team.users.count < @team.number_player
     UserTeam.create!(user: current_user, team: @team)
-    redirect_to meet_path(@meet), notice: "Tu as rejoint l'équipe avec succès !"
+    return redirect_to meet_path(@meet), notice: "Tu as rejoint l'entraînement !"
+
+  # Cas 2 : Match
     else
-    redirect_to meet_path(@meet), alert: "Équipe complète !"
+    @match = @meetable
+    @team = Team.find(params[:team_id])
+
+    # Empêcher l'organisateur de changer d'équipe
+      if @match.user == current_user
+        return redirect_to meet_path(@meet), alert: "En tant qu'organisateur, tu es déjà chez les Bleus."
+      end
+
+    # Empêcher le double enregistrement (Bleu ou Rouge)
+      if @match.blue_team.users.include?(current_user) || @match.red_team.users.include?(current_user)
+        return redirect_to meet_path(@meet), alert: "Tu es déjà inscrit à ce match !"
+      end
+
+    # Vérification des places
+      if @team.users.count < @team.number_player
+      UserTeam.create!(user: current_user, team: @team)
+      redirect_to meet_path(@meet), notice: "Tu as rejoint l'équipe avec succès !"
+      else
+      redirect_to meet_path(@meet), alert: "Équipe complète !"
+      end
     end
   end
 
   def leave
-    @meet = Meet.find(params[:id])
-    @match  = @meet.meetable
-    # je trouve l'association entre l'utilisateur et l'équipe (bleu ou rouge) pour pouvoir la supprimer
-    user_team = UserTeam.find_by(user: current_user, team: [ @match.blue_team, @match.red_team ])
+  @meet = Meet.find(params[:id])
+  @meetable = @meet.meetable
+
+    # On détermine l'équipe (ou les équipes) dans lesquelles chercher
+    if @meet.meetable_type == "Program"
+      # Pour un entraînement, on cherche dans l'équipe unique du programme
+      target_teams = @meetable.team
+    else
+      # Pour un match, on cherche dans les deux équipes
+      target_teams = [ @meetable.blue_team, @meetable.red_team ]
+    end
+
+    user_team = UserTeam.find_by(user: current_user, team: target_teams)
 
     if user_team
       user_team.destroy
-      redirect_to meet_path(@meet), notice: "Tu as quitté l'équipe avec succès !"
+      redirect_to meet_path(@meet), notice: "Vous avez quitté la session avec succès !"
     else
-      redirect_to meet_path(@meet), alert: "Tu n'es pas inscrit à ce match !"
+      redirect_to meet_path(@meet), alert: "Vous n'êtes pas inscrit à cette rencontre !"
     end
   end
 
@@ -80,7 +105,7 @@ class MeetsController < ApplicationController
     @meet = @program.meets.build(meet_params)
 
     if @meet.save
-      redirect_to @program
+      redirect_to meet_path(@meet), notice: "Votre entraînement a été partagé !"
     else
       render :new, status: :unprocessable_entity
     end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,8 +1,16 @@
 class Program < ApplicationRecord
   LEVELS = [ "Debutant", "Intermediate", "Confirmed", "Expert" ]
   belongs_to :user
-  belongs_to :team
+  belongs_to :team, optional: true #l'équipe qui contient les participants
   has_many :meets, as: :meetable
   validates :level, inclusion: { in: LEVELS }
   validates :goal, presence: true
+
+  # Si pas d'équipe, on est créer une
+  before_validation :ensure_team_exists, on: :create
+
+  private
+  def ensure_team_exists
+    self.team ||= Team.create(number_player: 99)
+  end
 end
