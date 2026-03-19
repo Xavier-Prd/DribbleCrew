830efe2bdf9283118654041ee3e9db4f15dbf8aa
Xavier Pardoue <pardoue.xavier@gmail.com>
2026-03-19 14:38:18 +0100

    Partager l'app dans le profil

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "maps", to: "maps#index", as: :maps
+  get "qrcode", to: "invites#qrcode", as: :qrcode
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
